3bf9aadecd76e0e768cc034c674525d7c6cde97d
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2017-08-31 16:34:55 +0530

    :dizzy: Plugin

---
+++ src/index.js
@@ -0,0 +1,37 @@
+module.exports = {
+
+	template: '<a v-show="sorttype === \'asc\'" @click="sortData(data)">\
+		{{ label }} <i class="down black icon" :class="icon" v-if="sort === 1"></i>\
+	</a>\
+	<a v-show="sorttype === \'desc\'" @click="sortData(data)">\
+		{{ label }} <i class="up black icon" :class="icon" v-if="sort === 1"></i>\
+	</a>',
+
+	props: {
+    label: {
+      type: String,
+      default: 'Sort Me',
+      required: false
+    },
+    icon: {
+      type: String,
+      default: 'chevron',
+      required: false
+    },
+    data: {
+    	type: Object,
+			default: function() {
+				return {
+					sort: null,
+					sorttype: null,
+				}
+			},
+			required: true
+		}
+  },
+	methods: {
+		sortData(sort, direction) {
+			this.$emit('sort-data', sort, direction);
+		},
+	}
+}
