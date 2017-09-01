2c4d4959e6e7abad627a4404b96244817bb60e6c
Vinayak Kulkarni <vinayak@shoptreeapp.com>
2017-09-01 13:30:57 +0530

    Fixed the icon visibility

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ module.exports = {
 
 	template: '<div>\
 		<router-link v-if="data.sorttype === \'desc\'" :to="{ name: routeName, query: { sort: data.sort, sorttype: data.sorttype }}" @click.native="sortData(data.sort, data.sorttype)">\
-			{{ label }} <i class="chevron down black icon" v-if="data.sort"></i>\
+			{{ label }} <i class="down black icon" :class="icon" v-if="data.sort === this.$route.query.sort"></i>\
 		</router-link>\
 		<router-link v-if="data.sorttype === \'asc\'" :to="{ name: routeName, query: { sort: data.sort, sorttype: data.sorttype }}" @click.native="sortData(data.sort, data.sorttype)">\
-			{{ label }} <i class="chevron up black icon" v-if="data.sort"></i>\
+			{{ label }} <i class="up black icon" :class="icon" v-if="data.sort === this.$route.query.sort"></i>\
 		</router-link>\
 	</div>',
 
